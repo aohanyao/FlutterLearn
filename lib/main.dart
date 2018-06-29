db315ccec9bc9c7d5a6586b897646ced55877080
aohanyao <aohanyao@gmail.com>
2018-06-29 20:50:48 +0800

    写了个列表
    写了个列表

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(new MainApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+      title: "Flutter Learn",
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Home"),
+          actions: <Widget>[
+            new IconButton(icon: new Icon( Icons.list), onPressed: _pushSaved)
+          ],
+        ),
+        body: new RandomWord(),
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  //点击
+  void _pushSaved(){
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
   }
+}
+
+
+//相当于adapter
+class RandomWord extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new RandomWordState();
+  }
+
+}
+
+
+//相当于holder
+class RandomWordState extends State<RandomWord> {
+
+  //所有的单词
+  final _suggestions = <WordPair>[];
+
+  //单词的样式
+  final _bigerFont = const TextStyle(fontSize: 18.00);
+
+  //存储喜欢的单词
+  final _save = new Set<WordPair>();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return Scaffold(
+      body: _buildSuggestions(),
     );
   }
+
+
+  //创建
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, position) { //匿名函数
+
+        if (position.isOdd) { //奇数 加线
+          return new Divider(/*
+            height: 0.5,
+            color: Colors.black,
+          */);
+        }
+
+        // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
+        final index = position ~/ 2;
+        // 如果是建议列表中最后一个单词对
+        if (index >= _suggestions.length) {
+          // ...接着再生成10个单词对，然后添加到建议列表
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+
+  // 相当于 createView
+  Widget _buildRow(WordPair suggestion) {
+    final alreadySaved = _save.contains(suggestion);
+    return ListTile(
+      //标题
+      title: new Text(suggestion.asPascalCase,
+        style: _bigerFont,),
+      //尾标
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,),
+      //点击
+      onTap: (){
+        //设置状态，相当于 adapter.notificationDataChange()
+        setState(() {
+          if(alreadySaved){
+            _save.remove(suggestion);
+          }else{
+            _save.add(suggestion);
+          }
+        });
+      },
+    );
+  }
+
 }
